b82345c0f8fccb40652ceb1bfa73be2b588d00a5
kafka <1135436400@qq.com>
2026-06-22 15:18:29 +0800

    Fix：upstream tle.gpu.warp_specialize to original

--- python/triton/experimental/tle/language/gpu/core.py
+++ python/triton/experimental/tle/language/gpu/core.py
@@ -218,10 +218,9 @@ def warp_specialize(functions_and_args, worker_num_warps, worker_num_regs, _sema
     default_args = _as_call_args(default_args)
     default_block = builder.new_block()
     builder.set_insertion_point_to_start(default_block)
-    default_results = _generator.inline_JitFunction(default_fn, default_args, kwargs={})
+    default_results = _generator.call_JitFunction(default_fn, default_args, kwargs={})
     default_result_values = _as_result_values(default_results)
     default_result_handles = flatten_values_to_ir(default_result_values)
-    builder.set_insertion_point_to_end(default_block)
     builder.create_warp_yield(default_result_handles)
     result_types = [result.get_type() for result in default_result_handles]
 
@@ -245,8 +244,7 @@ def warp_specialize(functions_and_args, worker_num_warps, worker_num_regs, _sema
         block_args = [block.get_argument(remapped[j]) for j in builtins.range(len(flattened))]
         block_values = tuple(unflatten_ir_values(block_args, [arg.type for arg in worker_args]))
         caller_context = WarpSpecializeCallerContext(worker_num_warps[idx])
-        _generator.inline_JitFunction(worker_fn, block_values, kwargs={}, caller_context=caller_context)
-        builder.set_insertion_point_to_end(block)
+        _generator.call_JitFunction(worker_fn, block_values, kwargs={}, caller_context=caller_context)
         builder.create_warp_return()
 
     builder.set_insertion_point_after(ws_op.get_operation())
